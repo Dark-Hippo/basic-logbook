42fb9fc0afe6aa103f9ea75b78c6fc71f7e9666f
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-06-14 19:24:08 +0100

    Update to go back to logbook if in date specific entry list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faMoon as faMoonSolid } from '@fortawesome/free-solid-svg-icons'
 import { faMoon as faMoonRegular } from '@fortawesome/free-regular-svg-icons'
 import { useTheme } from '../context/ThemeContext';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 type HeaderProps = {
   back: boolean;
@@ -11,14 +11,17 @@ type HeaderProps = {
 
 export const Header = ({ back, title }: HeaderProps) => {
   const { theme, toggleTheme } = useTheme();
+  const { logbookId, date } = useParams<{ logbookId: string, date: string }>();
   const icon = theme === 'light' ? faMoonRegular : faMoonSolid;
+
+  const backLink = date ? `/logbook/${logbookId}` : '/';
 
   return (
     <header>
       <div className="left">
         {back &&
           <button>
-            <Link to="/">
+            <Link to={backLink}>
               <FontAwesomeIcon icon={faArrowLeft} />
             </Link>
           </button>}

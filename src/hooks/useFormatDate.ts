c30836f5b50fe815ff5dcf53634eb1c7dad10ffc
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-06-14 19:19:57 +0100

    Add function to format date in yyyy-mm-dd format as US date format is broken

--- src/hooks/useFormatDate.ts
+++ src/hooks/useFormatDate.ts
@@ -13,5 +13,12 @@ export const useFormatDate = () => {
       .replace(/\//g, '-');
   };
 
-  return formatDate;
+  const formatDateForURL = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
+  return { formatDate, formatDateForURL };
 };

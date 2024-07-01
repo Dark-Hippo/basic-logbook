42fe32ebee654d96461070a054a3ece68420b250
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-07-01 15:10:43 +0100

    Use new test data generation function

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import { ThemeProvider } from './context/ThemeContext.tsx';
 import { LogbookProvider } from './context/LogbookContext.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
-import { logbookData } from './testData.ts'
+import { testData } from './testData.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LogbookProvider initialLogbooks={logbookData}>
+    <LogbookProvider initialLogbooks={testData}>
       <ThemeProvider>
         <BrowserRouter>
           <App />

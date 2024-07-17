7408eca701e5655545a1454ba279070ec59ab573
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-07-17 13:16:25 +0100

    Basic page setup, get logbooks from selectedIds

--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,8 +1,22 @@
+import { useLocation } from "react-router-dom";
+import { useLogbook } from "../context/LogbookContext";
+import { useEffect } from "react";
+
 export const Compare = () => {
+  const location = useLocation();
+  const selectedIds = location.state?.selectedIds as number[] || [];
+
+  const { logbooks } = useLogbook();
+  const selectedLogbooks = logbooks.filter(logbook => selectedIds.includes(logbook.id));
 
   return (
     <div>
-      Comparison
+      Ids are:
+      {selectedIds.map(id => (
+        <div key={id}>
+          {id}
+        </div>
+      ))}
     </div>
   )
 }

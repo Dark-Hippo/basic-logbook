8f01148b0625d39e94e16712eaeb65a78eddfac5
Tom Odell <Dark-Hippo@users.noreply.github.com>
2024-10-30 14:30:58 +0000

    Revamp test data generator function

--- src/testData.ts
+++ src/testData.ts
@@ -73,16 +73,25 @@ export const logbookData: LogBookType[] = [
   },
 ];
 
+interface testDataProps {
+  logbookName: string;
+  startDate: Date;
+  endDate: Date;
+  minRecords: number;
+  maxRecords: number;
+  minValue: number;
+  maxValue: number;
+}
 // function to generate test data, takes in a start and end date, min and max number of records to generate for each date and min and max values for each record
-export const generateTestData = (
-  logbookName: string,
-  startDate: Date,
-  endDate: Date,
-  minRecords: number,
-  maxRecords: number,
-  minValue: number,
-  maxValue: number
-): LogBookType[] => {
+export const generateTestData = ({
+  logbookName,
+  startDate,
+  endDate,
+  minRecords,
+  maxRecords,
+  minValue,
+  maxValue,
+}: testDataProps): LogBookType[] => {
   const dateDiff = endDate.getTime() - startDate.getTime();
   const dateDiffDays = Math.ceil(dateDiff / (1000 * 60 * 60 * 24));
 
@@ -117,25 +126,39 @@ export const generateTestData = (
 const startDate = new Date();
 startDate.setDate(startDate.getDate() - 365);
 
-const proteinTestData = generateTestData(
-  'Protein Intake',
-  startDate,
-  new Date(),
-  1,
-  1,
-  150,
-  200
-);
+const testData1 = {
+  logbookName: 'Protein Intake',
+  startDate: startDate,
+  endDate: new Date(),
+  minRecords: 1,
+  maxRecords: 1,
+  minValue: 150,
+  maxValue: 200,
+};
+
+const startDate2 = new Date();
+startDate2.setDate(startDate2.getDate() - 30);
+const testData2 = {
+  logbookName: 'Protein Intake',
+  startDate: startDate2,
+  endDate: new Date(),
+  minRecords: 1,
+  maxRecords: 1,
+  minValue: 30,
+  maxValue: 50,
+};
+
+const proteinTestData = generateTestData(testData2);
 
 const waistSizeTestData = {
   id: 2,
   name: 'Waist Size',
   records: [
-    new LogbookRecord(new Date('2023-07-02T08:00:00Z'), 46),
-    new LogbookRecord(new Date('2023-10-03T08:00:00Z'), 45),
-    new LogbookRecord(new Date('2024-01-04T08:00:00Z'), 44),
-    new LogbookRecord(new Date('2024-04-05T08:00:00Z'), 42),
-    new LogbookRecord(new Date('2024-07-06T08:00:00Z'), 40),
+    new LogbookRecord(new Date('2024-10-02T08:00:00Z'), 46),
+    new LogbookRecord(new Date('2024-10-09T08:00:00Z'), 45),
+    new LogbookRecord(new Date('2024-10-16T08:00:00Z'), 44),
+    new LogbookRecord(new Date('2024-10-23T08:00:00Z'), 42),
+    new LogbookRecord(new Date('2024-10-30T08:00:00Z'), 40),
   ],
 };
 

6ac0dd3fe18bca0d63b0f476532fdcb57277a7ff
Tom Odell <Dark-Hippo@users.noreply.github.com>
2025-01-24 13:21:59 +0000

    Add react as import

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { AddNewLogbookModal } from "../components/AddNewLogbookModal"
 import { LogbookList } from "../components/LogbookList"

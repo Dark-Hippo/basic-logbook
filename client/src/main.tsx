8df50e7c9327072f785580112353e02172524bd5
Tom Odell <Dark-Hippo@users.noreply.github.com>
2025-01-28 17:59:09 +0000

    Add auth provider and AWS cognito config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,17 +4,28 @@ import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx';
 import { LogbookProvider } from './context/LogbookContext.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'react-oidc-context';
 
 import { testData } from './testData.ts'
 
+const cognitoAuthConfig = {
+  authority: `https://cognito-idp.${import.meta.env.VITE_COGNITO_REGION}.amazonaws.com/${import.meta.env.VITE_COGNITO_REGION}_${import.meta.env.VITE_COGNITO_USER_POOL_ID}`,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_AUTH_REDIRECT_URI,
+  response_type: "code",
+  scope: "phone openid email",
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LogbookProvider initialLogbooks={testData}>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </LogbookProvider>
+    <AuthProvider {...cognitoAuthConfig}>
+      <LogbookProvider initialLogbooks={testData}>
+        <ThemeProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </LogbookProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )

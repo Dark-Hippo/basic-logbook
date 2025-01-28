5c223c8115d71b90f3a86afe9711c12916725ff2
Tom Odell <Dark-Hippo@users.noreply.github.com>
2025-01-28 17:57:40 +0000

    Update to load env vars from correct file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,13 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, path.join(process.cwd(), '..'), '');
+  const env = loadEnv(mode, path.join(process.cwd(), '..'), 'VITE');
+
   return {
     plugins: [react()],
+    envDir: path.join(process.cwd(), '..'), // need this to load .env file in tsx files
     server: {
-      port: parseInt(env.CLIENT_PORT || '5173'),
+      port: parseInt(env.VITE_CLIENT_PORT || '5173'),
       host: true, // Allows connections from network, not just localhost
     },
     prettier: {

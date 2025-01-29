4aae3271fd6d9d9bcd53654e9663290661c4d82b
Tom Odell <Dark-Hippo@users.noreply.github.com>
2025-01-29 11:20:31 +0000

    Add basic burger menu

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,13 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faMoon as faMoonSolid } from '@fortawesome/free-solid-svg-icons'
-import { faMoon as faMoonRegular } from '@fortawesome/free-regular-svg-icons'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowLeft,
+  faMoon as faMoonSolid,
+  faBars,
+  faGear,
+  faRightToBracket
+} from '@fortawesome/free-solid-svg-icons';
+import { faMoon as faMoonRegular } from '@fortawesome/free-regular-svg-icons';
 import { useTheme } from '../context/ThemeContext';
 import { Link, useParams } from 'react-router-dom';
 
@@ -13,21 +20,45 @@ type HeaderProps = {
 
 export const Header = ({ back, title }: HeaderProps) => {
   const { theme, toggleTheme } = useTheme();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { logbookId, date } = useParams<{ logbookId: string, date: string }>();
-  const icon = theme === 'light' ? faMoonRegular : faMoonSolid;
-
+  const themeIcon = theme === 'light' ? faMoonRegular : faMoonSolid;
   const backLink = date ? `/logbook/${logbookId}` : '/';
 
   return (
     <header>
       <div className="left">
-        {back &&
+        {back && (
           <Link to={backLink}>
             <FontAwesomeIcon icon={faArrowLeft} />
-          </Link>}
+          </Link>
+        )}
       </div>
       <div className="middle">{title}</div>
-      <div className="right"><FontAwesomeIcon icon={icon} onClick={toggleTheme} /></div>
+      <div className="right">
+        <div className="menu-container">
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
+          {isMenuOpen && (
+            <div className="menu-dropdown">
+              <button onClick={toggleTheme}>
+                <FontAwesomeIcon icon={themeIcon} />
+                <span>Theme</span>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faGear} />
+                <span>Settings</span>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faRightToBracket} />
+                <span>Login</span>
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
